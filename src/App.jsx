c3288c794655811033b0ac6dbf7cd8dd729d6348
JawadAhmed07 <qwertykey41@gmail.com>
2024-09-28 06:50:34 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,100 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Box, Button, colors, IconButton, TextField, Typography } from '@mui/material'
+import { Box, Button, CircularProgress, colors, IconButton, TextField, Typography } from '@mui/material'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { BorderOuterSharp, Javascript, SearchOutlined } from '@mui/icons-material'
 import Header from './components/Header'
+import { Masonry } from '@mui/lab';
 
 function App() {
 
-const [images,setImages]=useState();
-const [loading,setLoading]=useState(true);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
 
 
-useEffect(()=>{
-  getImagesFromUnsplash();
-},[])
+  useEffect(() => {
+    getImagesFromUnsplash();
+  }, [])
 
-const getImagesFromUnsplash= ()=>{
-  fetch(
-    "https://api.unsplash.com/photos/?client_id=CVa2Z-qUX4S_XCRrzlApSEO92QlzilwEmi-tE4JdfgM&per_page=30"
-  )
-  .then((res)=>res.json())
-  .then((res)=>
-  {
-    setImages(res)
-    setLoading(false)
+  const getImagesFromUnsplash = () => {
+    fetch(
+      "https://api.unsplash.com/photos/?page=1&client_id=CVa2Z-qUX4S_XCRrzlApSEO92QlzilwEmi-tE4JdfgM&per_page=30"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setImages(res)
+        setLoading(false)
+      }
+      )
   }
-
-)
-}
+  const searchImagesFromUnsplash = () => {
+    if (search) {
+      fetch(
+        `https://api.unsplash.com/search/photos/?page=1&query=${search}&client_id=CVa2Z-qUX4S_XCRrzlApSEO92QlzilwEmi-tE4JdfgM&per_page=30`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          console.log("search ka response: " , res)
+          setImages(res.results)
+          setLoading(false)
+        }
+        )
+    }
+  }
 
   return (
     <>
-    <div>
-      <Header/>
-    </div>
-      <div className='m-2 mx-5'>
-        {/* <Box display={"flex"} flexDirection={"row"} alignItems={"center"}
-          justifyContent={'space-between'}
-          bgcolor={"olivedrab"} padding={2}
-        >
-          <Typography className='text-white underline' variant='h5'>
-            Image Gallery
-          </Typography>
+      <div>
+        <Header />
+        <div className='m-2 mx-5'>
+          <Box display={"flex"} gap={2}
+            marginTop={2}
+            marginBottom={2}
+            paddingY={2}>
+            <TextField
+              id="outlined-basic"
+              label="Search Here"
+              color='secondary'
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              variant="outlined"
+              fullWidth />
 
-          <Button endIcon={<AccountCircleIcon />} variant="contained">Login</Button>
+            <Button variant="outlined"
+              endIcon={<SearchOutlined />}
+              color={"secondary"}
+              onClick={searchImagesFromUnsplash}
+              disabled={search === "" || search.length < 3} >
+              Search
+            </Button>
+          </Box>
+          {
+            loading ? (
+              <Box sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                margin: "10"
+              }}>
+                <CircularProgress color={"secondary"} size={60} />
+              </Box>
+            ) : null
+          }
 
-        </Box> */}
-        <Box display={"flex"} gap={2}
-         marginTop={2} 
-         marginBottom={2} 
-         paddingY={2}>
-          <TextField 
-          id="outlined-basic" 
-          label="Search Here" 
-          color='secondary'  
-          variant="outlined"
-          fullWidth />
-
-        <Button variant="outlined"
-          endIcon={<SearchOutlined />}
-          color={"secondary"} >
-          Search
-        </Button>
-        </Box>
+          <Masonry
+            columns={{ sm: 1, lg: 2, md: 3, xl: 4 }}
+            spacing={2}
+          >
+            {
+              images.map((item) => (
+                <img key={item.id} src={item.urls.regular} />
+              ))}
+          </Masonry>
+        </div>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
